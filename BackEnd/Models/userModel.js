1f517b3907baa2e0e5f53e507f944680866e227a
safiullah28 <safiullah424@gmail.com>
2024-10-07 19:59:58 +0500

    userModel created

--- BackEnd/Models/userModel.js
+++ BackEnd/Models/userModel.js
@@ -1,1 +1,26 @@
 const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+  create_at: Date,
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
