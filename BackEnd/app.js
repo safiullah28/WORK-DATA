de380f0486f66e2a560b085cb7ff49ba3918fbfd
safiullah28 <safiullah424@gmail.com>
2024-10-07 19:46:43 +0500

    mongodb connected

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -2,15 +2,24 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const router = require("./Routes/Route");
+const mongoose = require("mongoose");
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 app.use(express.json());
 app.use(cors());
-
 app.use("/api", router);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB", error);
+  });
