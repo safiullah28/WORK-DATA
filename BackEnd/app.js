a4dbf836d99fc16130dfb79ae72f05f7aae9b294
safiullah28 <safiullah424@gmail.com>
2024-10-07 19:31:16 +0500

    server created with MVC

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
-const app = express();
-const port = 3000;
+const cors = require("cors");
+const dotenv = require("dotenv");
+const router = require("./Routes/Route");
+dotenv.config();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const app = express();
+const port = process.env.PORT;
+app.use(express.json());
+app.use(cors());
+
+app.use("/api", router);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+  console.log(`Server is running on port ${port}`);
 });
